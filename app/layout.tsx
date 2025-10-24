5b56742ec232989c06e5671b5a198c7ef1b7c6f8
khil94 <zidru94@gmail.com>
2025-10-24 21:41:15 +0900

    feat : 기본 레이아웃 적용
    - provider, header, footer 배치

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import ThemeProvider from "./components/themeProvider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Footer from "./layouts/footer";
+import Header from "./layouts/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,12 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ko">
+      <body className={`antialiased`}>
         <ThemeProvider />
+        <Header
+          itemList={[
+            { name: "test-home", src: "/" },
+            { name: "test-home", src: "/" },
+            { name: "test-home", src: "/" },
+          ]}
+        />
         {children}
+        <Footer />
       </body>
     </html>
   );
