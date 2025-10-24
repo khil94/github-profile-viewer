669776668257565b0354af5a04810d457de654f1
khil94 <zidru94@gmail.com>
2025-10-24 23:44:48 +0900

    feat : 공용 SearchInput
    - onSubmit 핸들러 미부착 상태

---
+++ app/components/searchInput.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { MoveRight } from "lucide-react";
+import { useState } from "react";
+
+interface props {
+  className?: string;
+  onSubmit?: (val: string) => void;
+}
+
+export default function SearchInput({ className, onSubmit }: props) {
+  const [val, setVal] = useState("");
+
+  return (
+    <div className={`relative text-black ${className}`}>
+      <input
+        className={`w-full pr-[20%] md:pr-[18%] bg-white border-2 border-border rounded-4xl focus:outline-2 outline-accent-border p-[5%]`}
+        value={val}
+        onChange={(e) => {
+          setVal(e.target.value.trim());
+        }}
+      />
+      {val.length !== 0 && (
+        <MoveRight
+          // onClick={()=>onSubmit(val)}
+          className="hover:cursor-pointer w-[8%] md:w-[5%] h-full absolute opacity-60 top-1/2 -translate-y-1/2 right-[5%]"
+        />
+      )}
+    </div>
+  );
+}
