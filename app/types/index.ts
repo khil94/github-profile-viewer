6bbd803ae2fead8e7bca9269f5c67ec7388b935d
khil94 <zidru94@gmail.com>
2025-10-27 15:39:50 +0900

    feat : github user search 타입 설정
    - /search/user api 호출시 반환되는 데이터 타입 선언

--- app/types/index.ts
+++ app/types/index.ts
@@ -2,3 +2,31 @@ export interface HeadItem {
   name: string;
   src: string;
 }
+
+export interface GithubUserResponse {
+  incomplete_results: boolean;
+  items: GithubUserItem[];
+  total_count: number;
+}
+
+export interface GithubUserItem {
+  id: number;
+  login: string; // username
+  avatar_url: string; // avatar image url
+  node_id: string;
+  gravatar_id: string | null;
+  html_url: string; // profile url
+  score: number;
+  site_admin: boolean;
+  user_view_type: string;
+  url: string;
+  repos_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  organizations_url: string;
+  received_events_url: string;
+  subscriptions_url: string;
+  starred_url: string;
+  gists_url: string;
+}
