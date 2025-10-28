fb8d45f596f39722190431e660e9edb7834b97d3
khil94 <zidru94@gmail.com>
2025-10-28 18:55:05 +0900

    feat : search Input 1차 수정
    - 어차피 검색용도로 사용하는 Input이기에 Search 후 redirect 하는 기능을 Input에 종속시킴

--- app/components/searchInput.tsx
+++ app/components/searchInput.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { MoveRight } from "lucide-react";
-import { useState } from "react";
+import { redirect } from "next/navigation";
+import { HTMLProps, useState } from "react";
 
-interface props {
+interface props extends HTMLProps<HTMLDivElement> {
   className?: string;
-  onSubmit: (val: string) => void;
 }
 
-export default function SearchInput({ className, onSubmit }: props) {
+export default function SearchInput({ className, ...rest }: props) {
   const [val, setVal] = useState("");
 
   return (
-    <div className={`relative text-black ${className}`}>
+    <div {...rest} className={`relative text-black ${className}`}>
       <input
         className={`w-full pr-[20%] md:pr-[18%] bg-white border-2 border-border rounded-4xl focus:outline-2 outline-accent-border p-[5%]`}
         value={val}
@@ -22,7 +22,10 @@ export default function SearchInput({ className, onSubmit }: props) {
       />
       {val.length !== 0 && (
         <MoveRight
-          onClick={() => onSubmit(val)}
+          onClick={() => {
+            redirect(`/users/${val}`);
+            setVal("");
+          }}
           className="hover:cursor-pointer w-[8%] md:w-[5%] h-full absolute opacity-60 top-1/2 -translate-y-1/2 right-[5%]"
         />
       )}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import { API } from "./api/lib/api";
 import SearchInput from "./components/searchInput";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center">
-      <SearchInput
-        onSubmit={async (val: string) => {
-          const test = await API.searchUser(val);
-          console.log("test", test);
-        }}
-        className="w-4/5 md:w-2/3 md:text-2xl lg:w-1/2 lg:text-3xl 2xl:text-4xl"
-      />
+      <SearchInput className="w-4/5 md:w-2/3 md:text-2xl lg:w-1/2 lg:text-3xl 2xl:text-4xl" />
       {/* <section>TEST</section> */}
       {/* <section></section> */}
     </div>
