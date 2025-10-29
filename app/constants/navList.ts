1daebcdda2dd236de061aafc243748f3e43a8e99
khil94 <zidru94@gmail.com>
2025-10-29 14:27:21 +0900

    mockup : 목업용 데이터들

---
+++ app/constants/navList.ts
@@ -0,0 +1,19 @@
+import { HeadItem } from "../types";
+
+export const HomeNavList: HeadItem[] = [
+  {
+    name: "bookmarks",
+    src: "/bookmarks",
+  },
+];
+
+export const MainNavList: HeadItem[] = [
+  {
+    name: "bookmarks",
+    src: "/bookmarks",
+  },
+  {
+    name: "compare",
+    src: "/compare",
+  },
+];
