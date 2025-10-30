faa617c961c7470be72a347b387e4c0483b7705b
khil94 <zidru94@gmail.com>
2025-10-30 19:33:34 +0900

    feat : 누락 컴포넌트 -> numberWithIcon Component
    - 단순 컴포넌트

---
+++ app/components/numberWithIconComponent.tsx
@@ -0,0 +1,23 @@
+import { LucideIcon } from "lucide-react";
+
+export default function NumberWithIcon({
+  number,
+  Icon,
+  text,
+}: {
+  number: number;
+  Icon: LucideIcon;
+  text: string;
+}) {
+  return (
+    <div className="text-center flex flex-col justify-center items-center gap-2">
+      <h2 className=" font-bold text-2xl text-on-accent-primary">
+        {number.toLocaleString()}
+      </h2>
+      <div className="flex flex-row text-sm text-on-muted-primary gap-1">
+        <Icon className="w-[20%]" />
+        {text}
+      </div>
+    </div>
+  );
+}
