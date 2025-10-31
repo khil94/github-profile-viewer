b79273aa79b45e9a5366d9b73704a644fdc82b00
khil94 <zidru94@gmail.com>
2025-10-31 20:54:27 +0900

    style : contents layout items-center 적용

--- app/layouts/contents.tsx
+++ app/layouts/contents.tsx
@@ -4,7 +4,7 @@ interface props {
 
 export default function Contents({ children }: props) {
   return (
-    <div className="w-full flex justify-center p-24">
+    <div className="w-full flex justify-center items-center p-[5%]">
       <div className="max-w-4xl w-full">{children}</div>
     </div>
   );
