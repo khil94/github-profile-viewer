6e14bc0c49bd9d59f62b08b7afdb7c7a133299eb
khil94 <zidru94@gmail.com>
2025-10-31 17:51:09 +0900

    style : repoCard 스타일링

--- app/(main)/user/[id]/components/repoCard.tsx
+++ app/(main)/user/[id]/components/repoCard.tsx
@@ -14,8 +14,8 @@ export function TextWithIcon({
   text: string;
 }) {
   return (
-    <div>
-      <Icon />
+    <div className="flex flex-row gap-1 text-sm">
+      <Icon className="w-4" />
       <p>{text}</p>
     </div>
   );
@@ -23,8 +23,8 @@ export function TextWithIcon({
 
 export default function RepoCard({ repoData }: props) {
   return (
-    <Card className="rounded-2xl bg-primary-container p-2">
-      <h1 className="text-link font-bold text-xl">{repoData.name}</h1>
+    <Card className="rounded-2xl bg-primary-container pl-[5%]">
+      <h1 className="text-link font-bold text-lg">{repoData.name}</h1>
       <p className="text-on-muted-primary text-sm">{repoData.description}</p>
       <div className="flex flex-row items-center  gap-1 text-xs text-on-muted-primary">
         <CodeXml className="w-[5%] h-auto" />
@@ -32,7 +32,7 @@ export default function RepoCard({ repoData }: props) {
           return <p key={`${repoData.name}-${v.node.name}`}>{v.node.name}</p>;
         })}
       </div>
-      <div>
+      <div className="flex flex-row gap-2">
         <TextWithIcon
           Icon={Star}
           text={repoData.stargazerCount.toLocaleString()}
