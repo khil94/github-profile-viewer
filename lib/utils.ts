809617e1e42f9ca03ee8c4bebd35345b05854ff1
khil94 <zidru94@gmail.com>
2025-11-02 20:45:31 +0900

    feat : 현재 시간과의 차이를 문자열로 반환하는 함수
    - 최근/x분전/x시간전/일/주/개월/년 단위로 시간차를 반환하는 함수

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,3 +11,25 @@ export function dynamicColors() {
   const b = Math.floor(Math.random() * 255);
   return "rgb(" + r + "," + g + "," + b + ")";
 }
+
+export function getTimeAgo(target: string | Date) {
+  const now = new Date();
+  const targetDate = new Date(target);
+  const diff = now.getTime() - targetDate.getTime();
+
+  const sec = Math.floor(diff / 1000);
+  const min = Math.floor(sec / 60);
+  const hour = Math.floor(min / 60);
+  const day = Math.floor(hour / 24);
+  const week = Math.floor(day / 7);
+  const month = Math.floor(day / 30);
+  const year = Math.floor(day / 365);
+
+  if (year >= 1) return `${year}년 전`;
+  if (month >= 1) return `${month}개월 전`;
+  if (week >= 1) return `${week}주 전`;
+  if (day >= 1) return `${day}일 전`;
+  if (hour >= 1) return `${hour}시간 전`;
+  if (min >= 1) return `${min}분 전`;
+  return "최근";
+}
