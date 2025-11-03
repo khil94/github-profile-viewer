173c115a8b1fbfb04e3c17157f0eae11ae5eaa54
khil94 <zidru94@gmail.com>
2025-11-03 13:59:53 +0900

    feat : 최근 유저 섹션 userCard 클릭시 실제 해당 유저 상세 페이지로 이동

--- app/components/recentlyViewedSection.tsx
+++ app/components/recentlyViewedSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SearchX } from "lucide-react";
+import Link from "next/link";
 import { useRecentlyViewedStore } from "../store/recently_viewed/store";
 import NothingCardComponent from "./nothingCardComponent";
 import UserCardComponent from "./userCardComponent";
@@ -11,16 +12,16 @@ export default function RecentlyViewedSection() {
   return (
     <section className="min-h-[calc(100vh/2)] w-full items-center flex flex-col gap-6">
       <h3 className="text-2xl font-bold ">최근 본 사용자</h3>
-
       {userIdList.length > 0 ? (
         <div className="flex flex-col md:flex-row">
           {userIdList.map((v) => {
             return (
-              <UserCardComponent
-                handleRemove={(v) => deleteUser(v)}
-                key={v}
-                profile={userRecord[v]}
-              />
+              <Link key={v} href={`/user/${v}`}>
+                <UserCardComponent
+                  handleRemove={(v) => deleteUser(v)}
+                  profile={userRecord[v]}
+                />
+              </Link>
             );
           })}
         </div>
