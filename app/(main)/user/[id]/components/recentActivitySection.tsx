53b14878660cacad69f0ded2ffb7373f8065b77d
khil94 <zidru94@gmail.com>
2025-11-03 16:45:53 +0900

    style : 최근 활동 스타일링
    - 한줄로 병합
    - 아이콘이 항상 보이게 수정
    - 구조 변경

--- app/(main)/user/[id]/components/recentActivitySection.tsx
+++ app/(main)/user/[id]/components/recentActivitySection.tsx
@@ -11,33 +11,34 @@ export default function RecentActivitySection({ data }: props) {
   return (
     <section className="">
       <h3 className="text-2xl font-bold mb-6">최근 활동</h3>
-      <ul className="bg-primary-container rounded-2xl p-4">
-        {data.map((v, i) => {
-          const Icon = GithubEventsIcon[v.type];
+      <div className="p-6 bg-primary-container rounded-2xl">
+        <ul className="space-y-2">
+          {data.map((v, i) => {
+            const Icon = GithubEventsIcon[v.type];
 
-          return (
-            <li
-              className={`flex flex-row p-4 gap-4 ${
-                i !== data.length - 1 ? "border-b-1" : ""
-              }`}
-              key={v.createdAt}
-            >
-              <Icon className="text-accent-primary" />
-              <div className="flex flex-col gap-1">
-                <div className="flex flex-row gap-2">
-                  <p className="font-bold">{`${v.repo} -`}</p>
-                  <p className="text-on-muted-primary">{`${eventItemGenerator(
-                    v
-                  )}`}</p>
+            return (
+              <li
+                className={`flex gap-4 pb-4 border-b last:pb-0 last:border-b-0`}
+                key={v.createdAt}
+              >
+                <Icon className="text-accent-primary" />
+                <div className="flex-1 min-w-0">
+                  <p className="">
+                    <span className="font-bold">{`${v.repo}`}</span>
+                    {" - "}
+                    <span className="text-on-muted-primary">{`${eventItemGenerator(
+                      v
+                    )}`}</span>
+                  </p>
+                  <p className="text-on-muted-primary text-xs">
+                    {getTimeAgo(v.createdAt)}
+                  </p>
                 </div>
-                <p className="text-on-muted-primary text-xs">
-                  {getTimeAgo(v.createdAt)}
-                </p>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 }
