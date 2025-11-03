65b1505b2eeda0a9d1ad7f3309a7a25000bb6725
khil94 <zidru94@gmail.com>
2025-11-03 16:06:45 +0900

    style : contents pt-20 -> pt-30

--- app/layouts/contents.tsx
+++ app/layouts/contents.tsx
@@ -4,7 +4,7 @@ interface props {
 
 export default function Contents({ children }: props) {
   return (
-    <div className="w-full pt-20 min-h-svh flex justify-center items-start p-[5%]">
+    <div className="w-full pt-30 min-h-svh flex justify-center items-start p-[5%]">
       <div className="max-w-4xl w-full">{children}</div>
     </div>
   );
