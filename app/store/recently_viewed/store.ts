d5915f2a1b9ce300a471b7a46701cc68c8a76505
khil94 <zidru94@gmail.com>
2025-11-03 13:16:07 +0900

    feat : 최근 본 유저를 관리하는 store
    - 최근 상세 정보를 확인한 유저의 정보를 관리하는 store
    - key를 관리하는 userIdLIst와
    - key에 대응되는 item을 관리하는 userRecord로 분리
    - id가 존재한다면 순서를 가장 앞으로 옮김
    - max는 3개까지 저장

---
+++ app/store/recently_viewed/store.ts
@@ -0,0 +1,62 @@
+import { GithubUserItem } from "@/app/types";
+import { create } from "zustand";
+import { combine, persist } from "zustand/middleware";
+
+export const RECENTLY_VIEWED_USER = "github-profile-viewer-recently-viewed";
+
+const MAX_LIST_LENGTH = 3;
+
+export type recentlyViewedUserState = {
+  userRecord: Record<number, GithubUserItem>;
+  userIdList: number[];
+};
+const initialState: recentlyViewedUserState = {
+  userRecord: {},
+  userIdList: [],
+};
+
+export const useRecentlyViewedStore = create(
+  persist(
+    combine(initialState, (set, get) => ({
+      addUser: (target: GithubUserItem) => {
+        const { userIdList, userRecord } = get();
+        if (!userRecord[target.id]) {
+          if (userIdList.length !== MAX_LIST_LENGTH) {
+            const tempList = [...userIdList.slice(1), target.id];
+            const tempRecord = { ...userRecord, [target.id]: target };
+            set({
+              userIdList: tempList,
+              userRecord: tempRecord,
+            });
+          } else {
+            const deleteItem = userIdList[0];
+            const tempList = [...userIdList.slice(1), target.id];
+            const tempRecord = { ...userRecord };
+            delete tempRecord[deleteItem];
+            set({
+              userIdList: tempList,
+              userRecord: tempRecord,
+            });
+          }
+        } else {
+          // 존재하면 가장 최근에 본 것으로 위치 변경
+          const filtered = userIdList.filter((v) => v !== target.id);
+          set({ userIdList: [target.id, ...filtered] });
+        }
+      },
+      deleteUser: (targetId: number) => {
+        const { userIdList, userRecord } = get();
+        const tempList = userIdList.filter((v) => v !== targetId);
+        const tempRecord = { ...userRecord };
+        delete tempRecord[targetId];
+        set({
+          userIdList: tempList,
+          userRecord: tempRecord,
+        });
+      },
+    })),
+    {
+      name: RECENTLY_VIEWED_USER,
+    }
+  )
+);
