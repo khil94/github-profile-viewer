96f8e0bbe503148dc1cc39df85d155d7bc270acf
khil94 <zidru94@gmail.com>
2025-11-03 13:39:19 +0900

    style : 메인 페이지 스타일링 + 타이틀 문구 추가
    - placeholder 추가
    - 홈 페이지 타이틀 문구 추가

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,16 @@ export default function Home() {
 
   return (
     <div className="min-h-screen w-full flex flex-col items-center ">
-      <section className="min-h-screen w-full content-center">
+      <section className="min-h-screen w-full text-center content-center space-y-4">
+        <h1 className="text-4xl font-bold opacity-90">
+          Github 프로필을 분석해보세요
+        </h1>
+        <h2 className="text-on-muted-primary">
+          사용자의 저장소, 기여도, 통계를 한눈에 확인하세요
+        </h2>
         <SearchInput
           value={val}
+          placeholder="Github 사용자명을 입력하세요"
           onChangeValue={(v) => setVal(v)}
           onSubmit={(v) => redirect(`/users/${v}`)}
           className="w-4/5 md:w-2/3 m-auto md:text-2xl lg:w-1/2 lg:text-3xl 2xl:text-4xl"
