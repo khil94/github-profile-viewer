cd25e44417755ea467d3e2d382cf2aa8c2e13e43
khil94 <zidru94@gmail.com>
2025-11-03 13:30:56 +0900

    style : contents min-h-svh로 설정

--- app/layouts/contents.tsx
+++ app/layouts/contents.tsx
@@ -4,7 +4,7 @@ interface props {
 
 export default function Contents({ children }: props) {
   return (
-    <div className="w-full flex justify-center items-center p-[5%]">
+    <div className="w-full min-h-svh flex justify-center items-start p-[5%]">
       <div className="max-w-4xl w-full">{children}</div>
     </div>
   );
