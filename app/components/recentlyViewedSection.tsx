ffda24dd3b37b303762b8aef854e8c1cc07c5ade
khil94 <zidru94@gmail.com>
2025-11-03 13:29:43 +0900

    feat : 최근 본 사용자를 표현하는 섹션

---
+++ app/components/recentlyViewedSection.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { SearchX } from "lucide-react";
+import { useRecentlyViewedStore } from "../store/recently_viewed/store";
+import NothingCardComponent from "./nothingCardComponent";
+import UserCardComponent from "./userCardComponent";
+
+export default function RecentlyViewedSection() {
+  const { userIdList, userRecord, deleteUser } = useRecentlyViewedStore();
+
+  return (
+    <section className="min-h-[calc(100vh/2)] w-full items-center flex flex-col gap-6">
+      <h3 className="text-2xl font-bold ">최근 본 사용자</h3>
+
+      {userIdList.length > 0 ? (
+        <div className="flex flex-col md:flex-row">
+          {userIdList.map((v) => {
+            return (
+              <UserCardComponent
+                handleRemove={(v) => deleteUser(v)}
+                key={v}
+                profile={userRecord[v]}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <NothingCardComponent
+          className="bg-primary border-0"
+          TargetIcon={SearchX}
+          text="최근 본 사용자가 없습니다."
+        />
+      )}
+    </section>
+  );
+}
