50ca4bcc8f9d0cb82c3d5d10759cadca39980273
khil94 <zidru94@gmail.com>
2025-11-03 22:45:24 +0900

    chore : footer 제거

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import ThemeProvider from "../components/themeProvider";
 import Contents from "../layouts/contents";
-import Footer from "../layouts/footer";
 import Header from "../layouts/header";
 
 export default function RootLayout({
@@ -11,9 +10,8 @@ export default function RootLayout({
   return (
     <div>
       <ThemeProvider />
-      <Header itemList={[{ name: "bookmark", src: "/bookmark" }]} />
+      <Header />
       <Contents>{children}</Contents>
-      <Footer />
     </div>
   );
 }
